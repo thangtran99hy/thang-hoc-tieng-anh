90053ee856db6fad3f0dfc96f68d3444007dd4db
thangtran99hy <127844535+thangtran99hy@users.noreply.github.com>
2023-04-06 14:46:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,67 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-import Music from './pages/Music';
-import { BookFilled , VideoCameraFilled, PlayCircleFilled, CustomerServiceFilled, CheckCircleFilled  } from '@ant-design/icons';
-import { Tabs } from 'antd';
-import Video from './pages/Video';
-import Movies from './pages/Movies';
-import Book from './pages/Book';
-import EffortlessEnglish from './pages/EffortlessEnglish';
+import logo from "./logo.svg";
+import "./App.css";
+import Music from "./pages/Music";
+import {
+    BookFilled,
+    VideoCameraFilled,
+    PlayCircleFilled,
+    CustomerServiceFilled,
+    CheckCircleFilled,
+} from "@ant-design/icons";
+import { Tabs } from "antd";
+import Video from "./pages/Video";
+import Movies from "./pages/Movies";
+import Book from "./pages/Book";
+import EffortlessEnglish from "./pages/EffortlessEnglish";
 function App() {
-  const tabs = [
-  
-    {
-      code: 'music',
-      label: 'Music',
-      component: Music,
-      icon: CustomerServiceFilled
-    },
-    {
-      code: 'video',
-      label: 'Video',
-      component: Video,
-      icon: PlayCircleFilled
-    },
-    {
-      code: 'movies',
-      label: 'Movies',
-      component: Movies,
-      icon: VideoCameraFilled
-    },
-    {
-      code: 'book',
-      label: 'Book',
-      component: Book,
-      icon: BookFilled 
-    },
-    {
-      code: 'effortlessEnglish',
-      label: 'Effortless English',
-      component: EffortlessEnglish,
-      icon: CheckCircleFilled 
-    },
-  ]
-  return (
-    <Tabs
-    defaultActiveKey="2"
-    items={tabs.map((tab, i) => {
-      const id = String(i + 1);
-
-      return {
-        label: (
-          <div style={{
-            padding: 10,
-          }}>
-            <tab.icon />
-            <div>{tab.label}</div>
-          </div>
-        ),
-        key: id,
-        children: <tab.component />,
-      };
-    })}
-  />
-  );
+    const tabs = [
+        {
+            code: "music",
+            label: "Music",
+            component: Music,
+            icon: CustomerServiceFilled,
+        },
+        {
+            code: "video",
+            label: "Video",
+            component: Video,
+            icon: PlayCircleFilled,
+        },
+        {
+            code: "movies",
+            label: "Movies",
+            component: Movies,
+            icon: VideoCameraFilled,
+        },
+        {
+            code: "book",
+            label: "Book",
+            component: Book,
+            icon: BookFilled,
+        },
+        {
+            code: "effortlessEnglish",
+            label: "Effortless English",
+            component: EffortlessEnglish,
+            icon: CheckCircleFilled,
+        },
+    ];
+    return (
+        <Tabs
+            className="App_container"
+            items={tabs.map((tab, i) => {
+                return {
+                    label: (
+                        <div className="flex items-center px-2">
+                            <tab.icon />
+                            <div>{tab.label}</div>
+                        </div>
+                    ),
+                    key: tab.code,
+                    children: <tab.component />,
+                };
+            })}
+        />
+    );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    important: true,
+    mode: "jit",
+    content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
